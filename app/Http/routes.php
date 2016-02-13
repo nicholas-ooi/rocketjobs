06f2fae46f1856d1b64379bf1e1483bdb62e2d18
Hui Wen <huiwen_28@hotmail.com>
2016-02-13 11:11:43 -0800

    Homepage test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('sidebar');
 });
 
 Route::get('user/{id}', 'UserController@showProfile');
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.welcome')
+
+@section('sidebar')
+	@parent
+	<!-- Sidebar Menu -->
+	<div class="ui vertical visible inverted sidebar menu">
+	  <a class="active item">Home</a>
+	  <a class="item">Work</a>
+	  <a class="item">Company</a>
+	  <a class="item">Careers</a>
+	  <a class="item">Login</a>
+	  <a class="item">Signup</a>
+	</div>
+@endsection
