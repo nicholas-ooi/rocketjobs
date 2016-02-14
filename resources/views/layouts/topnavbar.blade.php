fcfef1b2c8cf1811b34a1d7324785acdd5cd9133
Hui Wen <huiwen_28@hotmail.com>
2016-02-14 12:32:49 -0800

    btn

--- resources/views/layouts/topnavbar.blade.php
+++ resources/views/layouts/topnavbar.blade.php
@@ -3,9 +3,9 @@
       <a class="item" href="{{url('/')}}">RocketJobs <i class="large rocket icon"></i></a>
       <div class="topnavbar right item">
           @if (!Auth::check())
-          <a class="ui inverted button" href="{{url('/login')}}">I'm Hiring!</a>
-        <a class="ui inverted button" href="{{url('/login')}}">Login</a>
-          <a class="ui inverted button" href="{{url('/register')}}">Register</a>
+          <a style="color: white;" class="ui inverted button" href="{{url('/login')}}">I'm Hiring!</a>
+        <a style="color: white;"  class="ui inverted button" href="{{url('/login')}}">Login</a>
+          <a style="color: white;"  class="ui inverted button" href="{{url('/register')}}">Register</a>
           @else
           <a class="ui inverted button" href="{{url('/logout')}}">Logout</a>
           @endif
