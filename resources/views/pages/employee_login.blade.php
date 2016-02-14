0594190ccb1d87dba223e460253085235c990dfb
Goh See Ting <kaibagoh@hotmail.com>
2016-02-13 16:51:35 -0800

    employee login fixed

--- resources/views/pages/employee_login.blade.php
+++ resources/views/pages/employee_login.blade.php
@@ -22,16 +22,6 @@
 <div class="pusher">
   <div class="ui inverted vertical masthead center aligned segment">
 
-   <div class="ui container">
-      <div class="ui large secondary inverted pointing menu">
-        <a class="item">RocketJobs</a>
-        <div class="right item">
-          <a class="ui inverted button">I'm Hiring!</a>
-          <a class="ui inverted button">Login/Sign Up</a>
-        </div>
-      </div>
-    </div>
-
     @include('layouts.topnavbar')
       <div class="ui raised very padded text container segment">
         <div class="ui two column middle aligned very relaxed stackable grid">
