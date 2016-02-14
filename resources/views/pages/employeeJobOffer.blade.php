f4de9cf81baf4aee6d8ff9af66c92a3e2b77a9f5
Hui Wen <huiwen_28@hotmail.com>
2016-02-14 10:29:24 -0800

    edit style job offers

--- resources/views/pages/employeeJobOffer.blade.php
+++ resources/views/pages/employeeJobOffer.blade.php
@@ -5,45 +5,43 @@
 @include('layouts.loginedUserNavbar')
 
 <div class="applicants ui container centered grid">
+	@foreach ($jobs as $job)
 	<div class="ui card five wide column">
-		@foreach ($jobs as $job)
-		<div>
-			<div class="content header">
-				<div class="header">{{$job->title}}</div>
-			</div>
+		<div class="content header">
+			<div class="header">{{$job->title}}</div>
+		</div>
 		@foreach ($job->users as $user)
 		@if ($user->pivot->status == "requesting" || $user->pivot->status == "approve" || $user->pivot->status == "decline")
 		<div class="content">
-				<h4 class="ui sub header">{{$user->name}}</h4>
-				<div class="ui small feed">
-					<div class="event">
-						<div class="content">
-							<div class="summary">
-								<div style="float:left;padding-left:10px;">
-								<p>Front end developer</p>
-								<p>{{$user->description}}</p>
-							</div>
-							<div style="clear:both;padding-top:10px;"></div>
-							</div>
+			<h4 class="ui sub header">{{$user->name}}</h4>
+			<div class="ui small feed">
+				<div class="event">
+					<div class="content">
+						<div class="summary">
+							<div style="float:left;padding-left:10px;">
+							<p>Front end developer</p>
+							<p>{{$user->description}}</p>
+						</div>
+						<div style="clear:both;padding-top:10px;"></div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="extra content">
-				<div class="ui two buttons">
-					@if ($user->pivot->status == "requesting")
-					<input type="button" class="ui basic green button" value="Requesting" />
-					@elseif ($user->pivot->status == "approve")
-					<input type="button" class="ui basic green button" value="Approved" />
-					@elseif ($user->pivot->status == "decline")
-					<input type="button" class="ui basic red button" value="Declined" />
-					@endif
-				</div>
+		</div>
+		<div class="extra content">
+			<div class="ui two buttons">
+				@if ($user->pivot->status == "requesting")
+				<input type="button" class="ui basic green button" value="Requesting" />
+				@elseif ($user->pivot->status == "approve")
+				<input type="button" class="ui basic green button" value="Approved" />
+				@elseif ($user->pivot->status == "decline")
+				<input type="button" class="ui basic red button" value="Declined" />
+				@endif
 			</div>
+		</div>
 		@endif
 		@endforeach
 	</div>
-		@endforeach
-	</div>
+	@endforeach
 </div>
 @endsection
