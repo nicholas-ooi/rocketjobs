e31ce9921cfdadb43900290365776cdf4bdc2655
Goh See Ting <kaibagoh@hotmail.com>
2016-02-14 07:45:58 -0800

    footer styled

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,12 +3,13 @@
 	<div class="ui inverted vertical footer segment">
     <div class="ui container">
       <div class="ui stackable inverted divided equal height stackable grid">
-			Copyright @ 2016 | Team Propulsion | All Rights Reserved
+			<h4 class = "copyright footer">Copyright @ 2016 | Team Propulsion | All Rights Reserved</h4>
 			<div class="power footer">
-			Powered by:
+				<h4>Powered by:</h4>
 				<img src="/testingsite/public/images/ibm.png">
 				<img src="/testingsite/public/images/magnet.jpg">
 				<img src="/testingsite/public/images/sparkpost.jpg">
+				<img src="/testingsite/public/images/cortical.jpg">
 			</div>
     </div>
   </div>
