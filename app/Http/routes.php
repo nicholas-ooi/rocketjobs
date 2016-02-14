f2bf92f39eb97bff9d04b791a3f4e70ba4eb1125
Goh See Ting <kaibagoh@hotmail.com>
2016-02-14 01:30:06 -0800

    ok

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/a', function () {
     return view('pages.home');
 });
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
 
 <!-- Page Contents -->
 <div class="pusher">
-  <div class="ui inverted vertical masthead center aligned segment">
+  <div class="homeone ui inverted vertical masthead center aligned segment">
 
     @include('layouts.topnavbar')
     <div class="ui text container">
@@ -19,15 +19,18 @@
     </div>
   </div>
 
+<div class="hometwo ui inverted vertical masthead center aligned segment">
+  <h1 class="home ui inverted header">How It Works?</h1>
   <div class="home-buttons ui two column grid centered center aligned">
-    <div class="row">
+    <div class="home row">
       <div class="four wide column">Search nearby rocket jobs</div>
       <div class="four wide column">Accept the offer</div>
     </div>
-    <div class="row">
+    <div class="home row">
       <div class="four wide column">Receive confirmation</div>
       <div class="four wide column">Happy Rocketjob-ing</div>
     </div>
   </div>
 </div>
+
 @endsection
