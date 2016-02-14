01de4cb3bbbd4c25501c8aa2005091c4236f2477
Nicholas Ooi <djfanaticx@gmail.com>
2016-02-14 03:07:22 -0800

    updated header login and logout

--- resources/views/layouts/followingnavbar.blade.php
+++ resources/views/layouts/followingnavbar.blade.php
@@ -6,12 +6,18 @@
       <div class="item">
         <a class="ui button">I'm Hiring!</a>
       </div>
+      @if (!Auth::check())
       <div class="item">
         <a class="ui button" href={{ url('/login') }}>Login</a>
       </div>
       <div class="item">
         <a class="ui button" href={{ url('/register') }}>Sign Up</a>
       </div>
+      @else
+      <div class="item">
+        <a class="ui button" href={{ url('/logout') }}>Logout</a>
+      </div>
+      @endif
     </div>
   </div>
 </div>
